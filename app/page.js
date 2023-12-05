3d601a6f5226cf138aece46a27194495ad6293f9
Daniel Chiquete <dchiquete2019@gmail.com>
2023-12-05 14:04:11 -0700

    update

--- app/page.js
+++ app/page.js
@@ -4,43 +4,6 @@ import Hero from "public/hero.png";
 import Button from "@/components/Button/Button";
 
 export default function Home() {
-  // const enhanced = id => {
-  //   const element = document.getElementById(id), 
-  //   text = element.innerText.split("");
-  //   element.innerText = "";
-  //   text.forEach(letter => {
-  //     const span = document.createElement("span");
-  //     span.className += "letter";
-  //     if(id == "channel-link1"){
-  //         span.className += " letter1";
-  //     }
-  //     if(id == "channel-link2"){
-  //         span.className += " letter2";
-  //     }
-  //     span.innerText = letter;
-  //     element.appendChild(span);
-  //     /* console.log(letter); */
-  //   });
-  // }
-  // enhanced("channel-link1");
-  // enhanced("channel-link2");
-
-  // var fancy = Array.from(document.getElementsByClassName("fancy"));
-  // fancy.forEach(element => {
-  //     element.addEventListener("mouseover", e => {
-  //         var intro = Array.from(document.getElementsByClassName("intro"));
-  //         intro.forEach(x =>{
-  //             x.style.opacity = "0.15";
-  //         });
-  //     });
-  //     element.addEventListener("mouseout", e => {
-  //         var intro = Array.from(document.getElementsByClassName("intro"));
-  //         intro.forEach(x =>{
-  //             x.style.opacity = "1.0";
-  //         });
-  //     });
-  // });
-
   return (
     <>
       <div className="section">
@@ -85,7 +48,6 @@ export default function Home() {
   //     <Image src={Hero} alt="" classNameName={styles.img} />
   //   </div>
   // </div>
-
   )
 }
 
